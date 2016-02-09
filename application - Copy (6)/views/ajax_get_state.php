1f682ee38d66a9b50693b13b50078900d62c5346
Projukti <sayantan.projukti@gmail.com>
2016-02-09 11:42:58 +0530

    add files

--- application - Copy (6)/views/ajax_get_state.php
+++
@@ -1,6 +0,0 @@
-<select name="select" onChange="getstatedetails(this.value)">
-<option value="" selected="selected" >Select State</option>
-<?php foreach($state as $stt): ?>
-<option value="<?php echo $stt->state_id; ?>"><?php echo $stt->state_name; ?></option>
-<?php endforeach; ?> 
-</select> 
